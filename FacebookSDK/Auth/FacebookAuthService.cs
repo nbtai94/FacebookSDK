d2c4222d729e9e018ed5634ac7bfd4c7927733cd
NGUYEN BAO TAI <btkt.1412@gmail.com>
2024-05-31 11:52:03 +0700

    add services

---
+++ FacebookSDK/Auth/FacebookAuthService.cs
@@ -0,0 +1,45 @@
+﻿namespace FacebookSDK.Auth
+{
+    public class FacebookAuthService
+    {
+        private readonly string _appId;
+        private readonly string _appSecret;
+        private readonly string _redirectUri;
+        private readonly string _appVersion;
+        private readonly List<string> _scopes;
+        public FacebookAuthService(string appId, string appSecret, string redirectUri, string appVersion, List<string> scopes)
+        {
+            _appId = appId;
+            _appSecret = appSecret;
+            _redirectUri = redirectUri;
+            _appVersion = appVersion;
+            _scopes = scopes;
+
+        }
+
+        public string GetLoginUrl()
+        {
+            return $"https://www.facebook.com/${_appVersion}/dialog/oauth?client_id={_appId}&redirect_uri={_redirectUri}&scope={_scopes}";
+        }
+
+        public async Task<string> GetAccessTokenAsync(string code)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync($"https://graph.facebook.com/{_appVersion}/oauth/access_token?client_id={_appId}&redirect_uri={_redirectUri}&client_secret={_appSecret}&code={code}");
+                var content = await response.Content.ReadAsStringAsync();
+                return content;
+            }
+        }
+
+        public async Task<string> GetUserInfoAsync(string accessToken)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync($"https://graph.facebook.com/me?access_token={accessToken}&fields=id,name,email");
+                var content = await response.Content.ReadAsStringAsync();
+                return content;
+            }
+        }
+    }
+}
---
+++ FacebookSDK/FacebookClient.cs
@@ -0,0 +1,36 @@
+﻿using FacebookSDK.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacebookSDK
+{
+    public class FacebookClient
+    {
+        private readonly string _accessToken;
+        public FacebookClient(string accessToken)
+        {
+            _accessToken = accessToken;
+        }
+
+        const string baseUrl = "https://graph.facebook.com";
+
+        public async Task<FacebookUser> GetUserProfileAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetStringAsync($"{baseUrl}/me?fields=id,name,email&access_token={_accessToken}");
+                var json = JObject.Parse(response);
+                return new FacebookUser
+                {
+                    Id = json["id"].ToString(),
+                    Name = json["name"].ToString(),
+                    Email = json["email"]?.ToString()
+                };
+            }
+        }
+    }
+}
